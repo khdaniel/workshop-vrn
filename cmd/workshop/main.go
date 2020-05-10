9eb00d9ef25e95680131c8fba0006fe3e760b9a2
khdaniel <daniel.khassanov@outlook.com>
2020-05-11 01:32:16 +0300

    added graceful shutdown + ENV variables

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/ilyakaznacheev/cleanenv"
@@ -14,6 +19,8 @@ import (
 
 func main() {
 
+	//ctx := context.Background()
+
 	cfg := config.Server{}
 	err := cleanenv.ReadConfig("config.yml", &cfg)
 	if err != nil {
@@ -22,17 +29,34 @@ func main() {
 
 	apiClient := jokes.NewJokeClient(cfg.JokeURL)
 
-	h := handler.NewHandler(apiClient)
+	h := handler.NewHandler(apiClient, cfg.CustomJoke)
 
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
 
 	path := cfg.Host + ":" + cfg.Port
-	log.Printf("Starting server at %s", path)
-	err = http.ListenAndServe(path, r)
-	if err != nil {
-		log.Fatal(err)
+	srv := http.Server{
+		Addr:    path,
+		Handler: r,
 	}
-	log.Print("Shutting server down")
+
+	quit := make(chan os.Signal, 1)
+	done := make(chan error, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+		err := srv.Shutdown(ctx)
+		done <- err
+	}()
+
+	log.Printf("Starting server at %s", path)
+
+	_ = srv.ListenAndServe()
+
+	err = <-done
+
+	log.Printf("Shutting server down with %v", err)
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -8,15 +8,22 @@ import (
 
 type Handler struct {
 	JokeClient api.Client
+	customJoke string
 }
 
-func NewHandler(jokeClient api.Client) *Handler {
+func NewHandler(jokeClient api.Client, customJoke string) *Handler {
 	return &Handler{
 		JokeClient: jokeClient,
+		customJoke: customJoke,
 	}
 }
 
 func (h *Handler) Hello(w http.ResponseWriter, r *http.Request) {
+	if h.customJoke != "" {
+		fmt.Fprint(w, h.customJoke)
+		return
+	}
+
 	joke, err := h.JokeClient.GetJoke()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
