f339e47df0f16c26a3d22fe11d8fb117ab1ced95
khdaniel <daniel.khassanov@outlook.com>
2020-05-10 19:32:09 +0300

    fixed host:port yamlkeys in config.go

--- internal/handler/config/config.go
+++ internal/handler/config/config.go
@@ -2,6 +2,6 @@ package config
 
 // Server config
 type Server struct {
-	Host string `yaml:"host" env:"PORT"`
-	Port string `yaml:"port" env:"host" env-default:"0.0.0.0"`
+	Host string `yaml:"host" env:"HOST"`
+	Port string `yaml:"port" env:"PORT" env-default:"0.0.0.0"`
 }
